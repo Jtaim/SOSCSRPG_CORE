475798f23bca93768a3e86795eb7c5a5d57992f8
Jtaim <jtaimneas@gmail.com>
2020-04-25 13:23:45 -0400

    Displaying the player object

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,19 +1,24 @@
-﻿using Engine.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
-            CurrentPlayer = new Player();
-            CurrentPlayer.Name = "James";
-            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer = new Player {
+                Name = "James",
+                CharacterClass = "Fighter",
+                HitPoints = 10,
+                Gold = 1000000,
+                ExperiencePoints = 0,
+                Level = 1
+            };
         }
     }
 }
