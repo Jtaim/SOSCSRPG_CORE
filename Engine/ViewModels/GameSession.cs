70c18c33242441a550ea7d3b5ba278d0236ab1c3
Jtaim <jtaimneas@gmail.com>
2020-04-25 11:48:55 -0400

    Added view model class

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "James";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
