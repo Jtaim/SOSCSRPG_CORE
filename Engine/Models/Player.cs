9dae5a63db47ced3a30be0e4020953c11ec3e7ef
Jtaim <jtaimneas@gmail.com>
2020-05-09 16:25:28 -0400

    Using Quests in the game.

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -56,7 +56,12 @@ namespace Engine.Models
 
         public ObservableCollection<GameItem> Inventory { get; set; }
 
+        public ObservableCollection<QuestStatus> Quests { get; set; }
+
         public Player()
-            => Inventory = new ObservableCollection<GameItem>();
+        {
+            Inventory = new ObservableCollection<GameItem>();
+            Quests = new ObservableCollection<QuestStatus>();
+        }
     }
 }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Factories;
+﻿using System.Linq;
+using Engine.Factories;
 using Engine.Models;
 
 namespace Engine.ViewModels
@@ -19,6 +20,8 @@ namespace Engine.ViewModels
                 OnPropertyChanged(nameof(HasLocationToWest));
                 OnPropertyChanged(nameof(HasLocationToEast));
                 OnPropertyChanged(nameof(HasLocationToSouth));
+
+                GivePlayerQuestsAtLocation();
             }
         }
         public bool HasLocationToNorth
@@ -74,6 +77,15 @@ namespace Engine.ViewModels
         {
             if(HasLocationToSouth) {
                 CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+            }
+        }
+
+        private void GivePlayerQuestsAtLocation()
+        {
+            foreach(Quest quest in CurrentLocation.QuestsAvailableHere) {
+                if(!CurrentPlayer.Quests.Any(q => q.PlayerQuest.ID == quest.ID)) {
+                    CurrentPlayer.Quests.Add(new QuestStatus(quest));
+                }
             }
         }
     }
