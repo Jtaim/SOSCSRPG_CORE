11b9c591c005d2343a305d67fd2566e392269ab5
Jtaim <jtaimneas@gmail.com>
2020-05-09 17:05:34 -0400

    Adding Monsters to Locations

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -7,6 +7,7 @@ namespace Engine.ViewModels
     public class GameSession : BaseNotificationClass
     {
         private Location _currentLocation;
+        private Monster _currentMonster;
 
         public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
@@ -22,8 +23,20 @@ namespace Engine.ViewModels
                 OnPropertyChanged(nameof(HasLocationToSouth));
 
                 GivePlayerQuestsAtLocation();
+                GetMonsterAtLocation();
             }
         }
+
+        public Monster CurrentMonster {
+            get { return _currentMonster; }
+            set {
+                _currentMonster = value;
+
+                OnPropertyChanged(nameof(CurrentMonster));
+                OnPropertyChanged(nameof(HasMonster));
+            }
+        }
+
         public bool HasLocationToNorth
             => CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
 
@@ -35,6 +48,9 @@ namespace Engine.ViewModels
 
         public bool HasLocationToSouth
             => CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+
+        public bool HasMonster
+            => CurrentMonster != null;
 
         public GameSession()
         {
@@ -88,5 +104,8 @@ namespace Engine.ViewModels
                 }
             }
         }
+
+        private void GetMonsterAtLocation()
+            => CurrentMonster = CurrentLocation.GetMonster();
     }
 }
